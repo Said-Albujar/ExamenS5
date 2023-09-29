ea7e12b12e60d4443fa255b597e2ce0fce98439f
Said Albujar Torres <saidalbujar23@gmail.com>
2023-09-28 21:19:23 -0500

    Tienda

---
+++ ExamenS5/ExamenS5/Tienda.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenS5
+{
+    internal class Tienda
+    {
+        private List<Producto> productos = new List<Producto>();
+
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        public double CalcularTotal()
+        {
+            double total = 0;
+            foreach (var producto in productos)
+            {
+                total += producto.Precio;
+            }
+            return total;
+        }
+
+        public void MostrarContenido()
+        {
+            Console.WriteLine("Contenido del carrito de compras:");
+            foreach (var producto in productos)
+            {
+                Console.WriteLine($"{producto.ObtenerCaracteristicas()}");
+            }
+            Console.WriteLine($"Precio Total: {CalcularTotal():C}");
+        }
+
+        public void LimpiarCarrito()
+        {
+            productos.Clear();
+        }
+    }
+}
